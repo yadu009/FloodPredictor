97c2e62e2e20fa92c52dc0ad2109d30510776609
yadu009 <123614663+yadu009@users.noreply.github.com>
2025-10-28 08:40:30 +0530

    Delete static directory

--- static/contact.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(e){
-    e.preventDefault();
-    alert("Thank you! Your message has been submitted.");
-    this.reset();
-});
